3ae85d79ae4210e0f0a914f83274c875c4b57c48
Clint Johnson <clint.johnson@jasper.ai>
2022-12-10 14:32:25 -0700

    Cowculator UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img className="cow" src="./cow.jpeg" alt="cow" />
+      <div className="calculator-container">
+        <div className="display-container">
+          <div className="display"></div>
+          <div className="number-operator-container">
+            <div className="numbers-container">
+              <div>
+                <Button>Clear</Button>
+              </div>
+              <div>
+                <Button>7</Button>
+                <Button>8</Button>
+                <Button>9</Button>
+              </div>
+              <div>
+                <Button>4</Button>
+                <Button>5</Button>
+                <Button>6</Button>
+              </div>
+              <div>
+                <Button>1</Button>
+                <Button>2</Button>
+                <Button>3</Button>
+              </div>
+              <div>
+                <Button>0</Button>
+                <Button>.</Button>
+              </div>
+            </div>
+            <div className="operators-container">
+              <Button>+</Button>
+              <Button>-</Button>
+              <Button>*</Button>
+              <Button>/</Button>
+              <Button>=</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+import "../App.css";
+
+export const Button = (props) => {
+  return <button className="button">{props.children}</button>;
+};
